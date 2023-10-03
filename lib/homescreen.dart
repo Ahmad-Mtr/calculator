5ca0a4e1873b10e0337086da70640125245b76e0
AhmadMtr <ahmadmtr@protonmail.com>
2023-10-04 00:41:26 +0300

    ..

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   String number1 = "";
   String operand = "";
   String number2 = "";
-  bool isDarkMode = true;
+  bool isDarkMode = false;
   late List<bool> isPressedList; // Track the pressed state for each button
 
   @override
